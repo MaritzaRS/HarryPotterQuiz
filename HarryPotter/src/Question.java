957ff9bd27ad23f810917815112fb42caf166882
1861496 <1861496@01-18133.pusd.dom>
2025-05-16 14:00:04 -0700

    Set the question ArrayList to Question class.

--- HarryPotter/src/Question.java
+++ HarryPotter/src/Question.java
@@ -6,9 +6,17 @@ public class Question {
 	private String gryffAnswer;
 	private String slythAnswer;
 	
+//	private boolean addHuff;		//returns true if certain answer is selected
+//	private boolean addGryff;
+//	private boolean addRaven;
+//	private boolean addSlyth;
 	
-	public Question(String text) {
+	public Question(String text, String h, String g, String r, String s) {
 		question = text;
+		huffAnswer = h;
+		ravenAnswer = r;
+		gryffAnswer = g;
+		slythAnswer = s;
 		
 	}
 
@@ -18,6 +26,38 @@ public class Question {
 
 	public void setQuestion(String question) {
 		this.question = question;
+	}
+
+	public String getHuffAnswer() {
+		return huffAnswer;
+	}
+
+	public void setHuffAnswer(String huffAnswer) {
+		this.huffAnswer = huffAnswer;
+	}
+
+	public String getRavenAnswer() {
+		return ravenAnswer;
+	}
+
+	public void setRavenAnswer(String ravenAnswer) {
+		this.ravenAnswer = ravenAnswer;
+	}
+
+	public String getGryffAnswer() {
+		return gryffAnswer;
+	}
+
+	public void setGryffAnswer(String gryffAnswer) {
+		this.gryffAnswer = gryffAnswer;
+	}
+
+	public String getSlythAnswer() {
+		return slythAnswer;
+	}
+
+	public void setSlythAnswer(String slythAnswer) {
+		this.slythAnswer = slythAnswer;
 	}
 	
 	
--- HarryPotter/src/Quiz.java
+++ HarryPotter/src/Quiz.java
@@ -7,11 +7,10 @@ import javax.swing.JPanel;
 public class Quiz {
 	
 	JFrame frame;
-	private static ArrayList<String> question;
+	private static ArrayList<Question> question;
 	
 	public Quiz() {
-		question = new ArrayList<String>();
-		question.add("What result are you hoping for?");
+		question = new ArrayList<Question>();
 		
 		setup();
 		
@@ -20,29 +19,29 @@ public class Quiz {
 	public void setup() {
 		frame.setBounds(100, 100, 100, 100);
 		frame.addMouseListener(null);
-		setupBoard();
+		//setupBoard();
 	}
 	
-	public void setupBoard() {
-		JPanel jp = new JPanel();
-
-		GridLayout g = new GridLayout(8,8);
-		jp.setLayout(g);	
-
-
-//		Tile[][] board = chessLogic.getBoard();
-//		for(int i =0; i < question.size();i++) {
-//			for(int j = 0; j < question.size();j++) {
-//				jp.add(question.get(i));
-//				question.get(i).addMouseListener(this);
-//			}
-//		}
-		
-		frame.add(jp);
-		
-		
-		
-	}
+//	public void setupBoard() {
+//		JPanel jp = new JPanel();
+//
+//		GridLayout g = new GridLayout(8,8);
+//		jp.setLayout(g);	
+//
+//
+////		Tile[][] board = chessLogic.getBoard();
+////		for(int i =0; i < question.size();i++) {
+////			for(int j = 0; j < question.size();j++) {
+////				jp.add(question.get(i));
+////				question.get(i).addMouseListener(this);
+////			}
+////		}
+//		
+//		frame.add(jp);
+//		
+//		
+//		
+//	}
 	/*
 	 * hopefully graphically placing the quiz on a screen
 	 */
