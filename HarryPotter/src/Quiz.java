8f16dd6c0a88f42ac19994412b3a92513aa6cb2f
1861496 <1861496@01-18133.pusd.dom>
2025-05-16 13:26:00 -0700

    Started trying to set up. Trying being the key word.

---
+++ HarryPotter/src/Quiz.java
@@ -0,0 +1,52 @@
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Quiz {
+	
+	JFrame frame;
+	private static ArrayList<String> question;
+	
+	public Quiz() {
+		question = new ArrayList<String>();
+		question.add("What result are you hoping for?");
+		
+		setup();
+		
+	}
+	
+	public void setup() {
+		frame.setBounds(100, 100, 100, 100);
+		frame.addMouseListener(null);
+		setupBoard();
+	}
+	
+	public void setupBoard() {
+		JPanel jp = new JPanel();
+
+		GridLayout g = new GridLayout(8,8);
+		jp.setLayout(g);	
+
+
+//		Tile[][] board = chessLogic.getBoard();
+//		for(int i =0; i < question.size();i++) {
+//			for(int j = 0; j < question.size();j++) {
+//				jp.add(question.get(i));
+//				question.get(i).addMouseListener(this);
+//			}
+//		}
+		
+		frame.add(jp);
+	}
+
+	public static void main(String[] args) {
+		
+		new Quiz();
+		
+	}
+	
+	
+
+}
--- HarryPotter/src/Ravenclaw.java
+++ HarryPotter/src/Ravenclaw.java
@@ -1,8 +1,16 @@
+import java.awt.Color;
 
 public class Ravenclaw extends House{
 	
+	private String co;
+	private Color r;
+	
 	public Ravenclaw() {
 		super();
+		co = "studious";
+		r = Color.BLUE;
 	}
+	
+	
 	
 }
