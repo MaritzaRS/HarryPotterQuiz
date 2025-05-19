f9f9e67443ab148a0b18b8fe886368de4d8b1012
1930158 <1930158@01-18122.pusd.dom>
2025-05-19 12:41:36 -0700

    created slytherin class

---
+++ HarryPotter/src/Slytherin.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+
+public class Slytherin extends House{
+	
+	private String co;
+	private Color s;
+	
+	public Slytherin() {
+		super();
+		co = "idk what to put here";
+		s = Color.GREEN;
+	}
+	
+	
+	
+}
