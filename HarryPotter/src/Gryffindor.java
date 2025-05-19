b54dae42e117d6ef66696c351c424032f995cf4b
1930158 <1930158@01-18122.pusd.dom>
2025-05-19 12:40:43 -0700

    created gryffindor class

---
+++ HarryPotter/src/Gryffindor.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+
+public class Gryffindor extends House{
+	
+	private String co;
+	private Color g;
+	
+	public Gryffindor() {
+		super();
+		co = "idk what to put here";
+		g = Color.RED;
+	}
+	
+	
+	
+}
