9decfb58e435f62f15f7dec7f0a32c573eae7748
1930158 <1930158@01-18122.pusd.dom>
2025-05-19 12:39:48 -0700

    created hufflepuff class

---
+++ HarryPotter/src/Hufflepuff.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+
+public class Hufflepuff extends House{
+	
+	private String co;
+	private Color h;
+	
+	public Hufflepuff() {
+		super();
+		co = "idk what to put here";
+		h = Color.PINK;
+	}
+	
+	
+	
+}
